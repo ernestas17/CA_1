e3b8dacdfe992b8ed8f88284d54a8d306cadcc99
VaKamile <kjakubenaite@gmail.com>
2023-06-28 11:54:09 +0300

    added +/- logic

---
+++ src/components/atoms/Select/Select.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+interface IOptionProps {
+  value: string;
+  label: string | number;
+}
+
+const Select = () => {
+  const [options, setOptions] = useState<IOptionProps[]>([]);
+  const [selectedValue, setSelectedValue] = useState<string>('');
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedValue(value);
+  };
+
+  const populateDropdown = () => {
+    // Priklausomai nuo puslapio, kokių reikės duomenų
+    const fetchedOptions: IOptionProps[] = [
+      { value: '1', label: 'Option 1' },
+      { value: '2', label: 'Option 2' },
+      { value: '3', label: 100 },
+    ];
+    setOptions(fetchedOptions);
+  };
+
+  useEffect(() => {
+    populateDropdown();
+  }, []);
+
+  return (
+    <select value={selectedValue} onChange={handleSelectChange}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
