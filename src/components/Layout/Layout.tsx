c00d71d0a9110d28f29ad9e5c5cd69e1eb362073
VaKamile <kjakubenaite@gmail.com>
2023-06-28 13:41:52 +0300

    added Card skeleton & Bulma link in intex.html

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,13 @@ import Footer from '../Footer';
 import Header from '../Header';
 import { Outlet } from 'react-router-dom';
 const Layout = () => {
-    return (
-        <>
-            <Header></Header>
-            <Outlet />
-            <Footer></Footer>
-        </>
-    );
+  return (
+    <>
+      <Header></Header>
+      <Outlet />
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Layout;
---
+++ src/components/atoms/Card/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledCardWrapper = styled.div`
+  display: flex;
+  width: calc(100% / 5);
+  flex-wrap: wrap;
+
+  justify-content: center;
+`;
+export const StyledCardWrapperInside = styled.div`
+  display: flex;
+`;
+export const StyledCardContent = styled.div`
+  display: flex;
+`;
